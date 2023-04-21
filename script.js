7691b08303335f222f24cbf277e564feca4e4f5b
Radu Lefter <lefter_radu@yahoo.com>
2023-04-21 19:31:17 +0100

    experimented with text properties

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ window.addEventListener('load', ()=>{
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    const text = 'Hello';
+    const textX = canvas.width/2;
+    const textY = canvas.height/2;
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 3;
+    ctx.font = '80px Helevtica';
+    ctx.letterSpacing = '15px';
+    ctx.fillText(text, textX, textY);
+    ctx.strokeText(text, textX, textY);
 })
