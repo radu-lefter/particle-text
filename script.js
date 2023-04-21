8a132ff583399fc6f2180ce4566c78612c808062
Radu Lefter <lefter_radu@yahoo.com>
2023-04-21 20:03:26 +0100

    created function to render every word on new line

--- script.js
+++ script.js
@@ -17,17 +17,27 @@ window.addEventListener('load', ()=>{
     ctx.lineTo(canvas.width/2, canvas.height);
     ctx.stroke();
 
-    const text = 'Hello';
-    const textX = canvas.width/2;
-    const textY = canvas.height/2;
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'blue';
-    ctx.lineWidth = 3;
-
     ctx.font = '80px Helevtica';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.letterSpacing = '15px';
-    ctx.fillText(text, textX, textY);
-    ctx.strokeText(text, textX, textY);
+
+    const maxTextWidth = canvas.width * 0.5;
+
+
+    function wrapText(text){
+        let linesArray = [];
+        let lineCounter = 0;
+        let line = '';
+        let words = text.split(' ');
+        for(let i=0; i < words.length; i++){
+            let testLine = line + words[i] + ' ';
+            console.log(ctx.measureText(testLine).width);
+            ctx.fillText(testLine, canvas.width/2, canvas.height/2 + i * 70)
+        }
+        
+    }
+
+    wrapText('Hello, how are you');
 })
