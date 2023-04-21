203c019cedae1f8b9ba55fcf2f3afe9897fa7b94
Radu Lefter <lefter_radu@yahoo.com>
2023-04-21 19:20:05 +0100

    set canvas to wondows size

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', ()=>{
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+})
