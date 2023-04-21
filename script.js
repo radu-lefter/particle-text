a3c0334f26de1d43e5d3a2dc16000840fc628af9
Radu Lefter <lefter_radu@yahoo.com>
2023-04-21 22:43:52 +0100

    added gradient and font to effect class

--- script.js
+++ script.js
@@ -1,110 +1,107 @@
-window.addEventListener('load', ()=>{
-    const textInput = document.getElementById('textInput');
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+window.addEventListener('load', () => {
+  const textInput = document.getElementById('textInput');
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    class Particle {
-        constructor(){
+  class Particle {
+    constructor() {}
 
-        }
+    draw() {}
 
-        draw(){
+    update() {}
+  }
 
-        }
-
-        update(){
-
-        }
+  class Effect {
+    constructor(context, canvasWidth, canvasHeight) {
+      this.context = context;
+      this.canvasWidth = canvasWidth;
+      this.canvasHeight = canvasHeight;
+      this.textX = this.canvasWidth / 2;
+      this.textY = this.canvasHeight / 2;
+      this.fontSize = 100;
     }
 
-    class Effect {
-        constructor(context, canvasWidth, canvasHeight){
-            this.context = context;
-            this.canvasWidth = canvasWidth;
-            this.canvasHeight = canvasHeight;
-            this.textX = this.canvasWidth/2;
-            this.textY = this.canvasHeight/2;
-
-        }
-
-        wrapText(text){
-            this.context.fillText(text, this.textX, this.textY);
-        }
-
-        convertToParticles(){
-            
-        }
-
-        render(){
-
-        }
+    wrapText(text) {
+      const gradient = this.context.createLinearGradient(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+      gradient.addColorStop(0.3, 'red');
+      gradient.addColorStop(0.5, 'orange');
+      gradient.addColorStop(0.7, 'yellow');
+      this.context.fillStyle = gradient;
+      this.context.font = this.fontSize + 'px Helvetica';
+      this.context.fillText(text, this.textX, this.textY);
     }
 
-    const effect = new Effect(ctx, canvas.width, canvas.height);
+    convertToParticles() {}
 
-    function animate(){
+    render() {}
+  }
 
-    }
+  const effect = new Effect(ctx, canvas.width, canvas.height);
+  effect.wrapText('Hello there how are you');
 
-    // ctx.lineWidth = 3;
-    // ctx.strokeStyle = 'red';
-    // ctx.beginPath();
-    // ctx.moveTo(0, canvas.height/2);
-    // ctx.lineTo(canvas.width, canvas.height/2);
-    // ctx.stroke();
+  function animate() {}
 
-    // ctx.strokeStyle = 'green';
-    // ctx.beginPath();
-    // ctx.moveTo(canvas.width/2, 0);
-    // ctx.lineTo(canvas.width/2, canvas.height);
-    // ctx.stroke();
+  // ctx.lineWidth = 3;
+  // ctx.strokeStyle = 'red';
+  // ctx.beginPath();
+  // ctx.moveTo(0, canvas.height/2);
+  // ctx.lineTo(canvas.width, canvas.height/2);
+  // ctx.stroke();
 
-    // const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-    // gradient.addColorStop(0.3, 'red');
-    // gradient.addColorStop(0.5, 'orange');
-    // gradient.addColorStop(0.7, 'yellow');
-    // ctx.fillStyle = gradient;
-    // ctx.strokeStyle = 'blue';
-    // ctx.font = '80px Helevtica';
-    // ctx.textAlign = 'center';
-    // ctx.textBaseline = 'middle';
+  // ctx.strokeStyle = 'green';
+  // ctx.beginPath();
+  // ctx.moveTo(canvas.width/2, 0);
+  // ctx.lineTo(canvas.width/2, canvas.height);
+  // ctx.stroke();
 
-    // const maxTextWidth = canvas.width * 0.8;
-    // const lineHeight = 80;
+  // const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+  // gradient.addColorStop(0.3, 'red');
+  // gradient.addColorStop(0.5, 'orange');
+  // gradient.addColorStop(0.7, 'yellow');
+  // ctx.fillStyle = gradient;
+  // ctx.strokeStyle = 'blue';
+  // ctx.font = '80px Helevtica';
+  // ctx.textAlign = 'center';
+  // ctx.textBaseline = 'middle';
 
+  // const maxTextWidth = canvas.width * 0.8;
+  // const lineHeight = 80;
 
+  // function wrapText(text){
+  //     let linesArray = [];
+  //     let lineCounter = 0;
+  //     let line = '';
+  //     let words = text.split(' ');
+  //     for(let i=0; i < words.length; i++){
+  //         let testLine = line + words[i] + ' ';
+  //         if(ctx.measureText(testLine).width > maxTextWidth){
+  //             line = words[i] + ' ';
+  //             lineCounter++;
+  //         } else {
+  //             line = testLine;
+  //         }
+  //         linesArray[lineCounter] = line;
+  //         console.log(ctx.measureText(testLine).width);
 
-    // function wrapText(text){
-    //     let linesArray = [];
-    //     let lineCounter = 0;
-    //     let line = '';
-    //     let words = text.split(' ');
-    //     for(let i=0; i < words.length; i++){
-    //         let testLine = line + words[i] + ' ';
-    //         if(ctx.measureText(testLine).width > maxTextWidth){
-    //             line = words[i] + ' ';
-    //             lineCounter++;
-    //         } else {
-    //             line = testLine;
-    //         }
-    //         linesArray[lineCounter] = line;
-    //         console.log(ctx.measureText(testLine).width);
-            
-    //     }
-    //     let textHeight = lineHeight * lineCounter;
-    //     let textY = canvas.height/2 - textHeight/2;
-    //     linesArray.forEach((el, index)=>{
-    //         ctx.fillText(el, canvas.width/2, textY + (index * lineHeight))
-    //     });
-    //     console.log(linesArray);
-        
-    // }
+  //     }
+  //     let textHeight = lineHeight * lineCounter;
+  //     let textY = canvas.height/2 - textHeight/2;
+  //     linesArray.forEach((el, index)=>{
+  //         ctx.fillText(el, canvas.width/2, textY + (index * lineHeight))
+  //     });
+  //     console.log(linesArray);
 
- 
-    // textInput.addEventListener('keyup', (e)=>{
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    //     wrapText(e.target.value);
-    // })
-})
+  // }
+
+  // textInput.addEventListener('keyup', (e)=>{
+  //     ctx.clearRect(0, 0, canvas.width, canvas.height)
+  //     wrapText(e.target.value);
+  // })
+});
