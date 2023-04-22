cb39f4e4f2079732caf99393d0efe5170060ff80
Radu Lefter <lefter_radu@yahoo.com>
2023-04-22 19:01:06 +0100

    rendered text to screen based on partciles array

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
     constructor(effect, x, y, color) {
       this.effect = effect;
       this.x = Math.random() * this.effect.canvasWidth;
-      this.y =y;
+      this.y = y;
       this.color = color;
       this.originX = x;
       this.originY = y;
@@ -26,7 +26,12 @@ window.addEventListener('load', () => {
 
     draw() {
       this.effect.context.fillStyle = this.color;
-      this.effect.context.fillRect(this.x, this.y, this.size, this.size);
+      this.effect.context.fillRect(
+        this.originX,
+        this.originY,
+        this.size,
+        this.size
+      );
     }
 
     update() {}
@@ -45,23 +50,22 @@ window.addEventListener('load', () => {
       this.textInput = document.getElementById('textInput');
       this.textInput.addEventListener('keyup', (e) => {
         this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-        if(e.key !== ' '){
-            this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-            this.wrapText(e.target.value);
+        if (e.key !== ' ') {
+          this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+          this.wrapText(e.target.value);
         }
       });
       this.particles = [];
-      this.gap = 3;
+      this.gap = 10;
       this.mouse = {
         radius: 20000,
         x: 0,
-        y:0
-      }
-      window.addEventListener('mousemove', (e)=>{
+        y: 0,
+      };
+      window.addEventListener('mousemove', (e) => {
         this.mouse.x = e.x;
         this.mouse.y = e.y;
-      })
-      
+      });
     }
 
     wrapText(text) {
@@ -119,12 +123,14 @@ window.addEventListener('load', () => {
         0,
         0,
         this.canvasWidth,
-        this.canvasHeight).data;
-      for(let y = 0; y < this.canvasHeight; y+=this.gap){
-        for(let x = 0; x < this.canvasWidth; x+=this.gap){
+        this.canvasHeight
+      ).data;
+      this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+      for (let y = 0; y < this.canvasHeight; y += this.gap) {
+        for (let x = 0; x < this.canvasWidth; x += this.gap) {
           const index = (y * this.canvasWidth + x) * 4;
           const alpha = pixels[index + 3];
-          if(alpha > 0){
+          if (alpha > 0) {
             const red = pixels[index];
             const green = pixels[index + 1];
             const blue = pixels[index + 2];
@@ -135,11 +141,17 @@ window.addEventListener('load', () => {
       }
     }
 
-    render() {}
+    render() {
+      this.particles.forEach((particle) => {
+        particle.update();
+        particle.draw();
+      });
+    }
   }
 
   const effect = new Effect(ctx, canvas.width, canvas.height);
   effect.wrapText('Hello there how are you');
+  effect.render();
 
   function animate() {}
 });
