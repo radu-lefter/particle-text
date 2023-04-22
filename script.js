e75f36771610cd93cc518c87b43fde41a53c75be
Radu Lefter <lefter_radu@yahoo.com>
2023-04-22 18:48:00 +0100

    created fields for particle class

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ window.addEventListener('load', () => {
   canvas.height = window.innerHeight;
 
   class Particle {
-    constructor() {}
+    constructor(effect, x, y, color) {
+      this.effect = effect;
+      this.x = Math.random() * this.effect.canvasWidth;
+      this.y =y;
+      this.color = color;
+      this.originX = x;
+      this.originY = y;
+      this.size = this.effect.gap;
+      this.dx = 0;
+      this.dy = 0;
+      this.vx = 0;
+      this.vy = 0;
+      this.force = 0;
+      this.angle = 0;
+      this.distance = 0;
+      this.friction = Math.random() * 0.6 + 0.15;
+      this.ease = Math.random() * 0.1 + 0.005;
+    }
 
     draw() {}
 
